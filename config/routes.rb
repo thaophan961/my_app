51707fbe2acdcbc920addf9da198c7fa64962345
Phan Thi Thu Thao <phan.thi.thu.thao@framgia.com>
2016-09-09 11:01:33 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 
   Rails.application.routes.draw do
 
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root 'static_pages#home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
