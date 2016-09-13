b6c69824198f23b414098bf9c3cfc8c7debffd2e
Phan Thi Thu Thao <phan.thi.thu.thao@framgia.com>
2016-09-13 08:54:31 +0700

    Finish log in/log out

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'jquery-rails', '4.0.3'
 gem 'turbolinks',   '2.3.0'
 gem 'jbuilder',     '2.2.3'
 gem 'sdoc',         '0.4.0', group: :doc
-
+gem 'pry-rails'
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
   gem 'byebug',      '3.4.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    debugger
   end
 
   def new
@@ -12,6 +11,7 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
+    	log_in @user
     	flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
